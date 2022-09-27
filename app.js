87ca0192180a0102e18743417bd062f1bc3b38ad
ZaryabAhmed22 <zaryabzak22@gmail.com>
2022-09-28 00:42:11 +0500

    Skeleton project complete

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const request = require("request");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//Setting the view enginr to ejs
+app.set("view engine", "ejs");
+
+// Creating a global variable for  todos
+let todos = [];
+
+app.get("/", function (req, res) {
+  options = { weekday: "long", month: "long", day: "numeric" };
+  let today = new Date();
+  let day = today.toLocaleDateString("en-US", options);
+
+  res.render("list", { kindOfDay: day, todos: todos });
+});
+
+app.post("/", function (req, res) {
+  console.log(req.body.todo);
+  todos.push(req.body.todo);
+
+  res.redirect("/");
+});
+
+app.listen(3000, function (req, res) {
+  console.log("Server live on port 3000");
+});
